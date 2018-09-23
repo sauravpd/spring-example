1526a74fd0d0dcd57af47d3cca0f5ca22c22c2fb
saurav <sauravpd58@gmail.com>
2018-09-24 01:14:12 +0530

    added config class

---
+++ src/main/java/spring_example/config/AppConfig.java
@@ -0,0 +1,19 @@
+package spring_example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ComponentScan({"spring_example"})
+@PropertySource("app.properties")
+public class AppConfig 
+{
+
+	@Bean
+	public PropertySourcesPlaceholderConfigurer getProperty() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
