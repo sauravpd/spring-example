855f3823a95c95e970f7dd5dcc6854a2b416a9ab
saurav <sauravpd58@gmail.com>
2018-09-24 01:20:37 +0530

    added javadoc

--- src/main/java/spring_example/service/CustomerService.java
+++ src/main/java/spring_example/service/CustomerService.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import spring_example.model.Customer;
 
+/**
+ * Customer Service
+ * @author saurav
+ *
+ */
 public interface CustomerService {
 
 	List<Customer> findAll();
