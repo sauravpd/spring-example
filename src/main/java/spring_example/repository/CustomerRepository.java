7dde2584ab17afc8081812739cbcc3b9fcb14573
saurav <sauravpd58@gmail.com>
2018-09-25 00:30:01 +0530

    commit changes in CustomerRepository interface

--- src/main/java/spring_example/repository/CustomerRepository.java
+++ src/main/java/spring_example/repository/CustomerRepository.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import spring_example.model.Customer;
 
+/**
+ * Customer Repository
+ * @author saurav
+ *
+ */
 public interface CustomerRepository 
 {
 	public List<Customer> findAll();
