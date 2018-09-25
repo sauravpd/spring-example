58bc6288db4b8576264e12627ece608f200b6533
saurav <sauravpd58@gmail.com>
2018-09-26 00:27:25 +0530

    added CustomerServiceImpl class

---
+++ src/main/java/spring_example/service/CustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package spring_example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import spring_example.model.Customer;
+import spring_example.repository.CustomerRepository;
+
+@Component("customerService")
+public class CustomerServiceImpl implements CustomerService 
+{
+
+	@Autowired
+	public CustomerRepository customerRepository;
+
+	@Override
+	public List<Customer> findAll()
+	{
+		return customerRepository.findAll();
+	}
+	
+}
