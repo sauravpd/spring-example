3bf8b64790b7b1fd18a8a63fa7d0cae0b5282f7f
saurav <sauravpd58@gmail.com>
2018-09-27 00:28:57 +0530

    added testconfig

---
+++ src/main/java/spring_example/config/testConfig.java
@@ -0,0 +1,11 @@
+package spring_example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"spring_example"})
+public class testConfig 
+{
+
+}
